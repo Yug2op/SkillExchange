c9e3e7c61501285e43e3ad216e53a2e9ac775223
Yugank Tripathi <mr.yugank.2000@gmail.com>
2025-10-23 03:13:56 +0530

    Update cookie settings in authController.js and restore API response interceptor logic in client.js
    - Modified cookie options in authController.js to set 'sameSite' to 'none' for cross-origin support in production.
    - Restored the error handling logic in client.js for 401 responses, ensuring user session management and redirection to the login page are active again.

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -140,8 +140,8 @@ export const login = async (req, res, next) => {
     const cookieOptions = {
       expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: `lax`
+      secure: process.env.NODE_ENV === 'production', 
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax' // none for cross-origin
     };
 
     res.cookie('token', token, cookieOptions);
